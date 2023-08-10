2bb076fff4f49fb4e7a14a1d4f90512d944b404c
Tom Mendy <114578691+Tom-Mendy@users.noreply.github.com>
2023-08-10 17:08:10 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -27,7 +27,7 @@ echo "TERMINAL"
 sudo nala install kitty -y
 
 echo "BASE-APP"
-sudo nala install vim tldr build-essential nm-tray network-manager pulseaudio pavucontrol bluez diodon neofetch htop thunar nitrogen -y
+sudo nala install vim tldr build-essential nm-tray network-manager pulseaudio pavucontrol bluez diodon neofetch htop thunar feh -y
 
 echo "bing wallpaper"
 mkdir -p ~/my_scripts
