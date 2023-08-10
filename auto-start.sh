a5f415826ae3c187f618cd795561810b7cbbb0b7
Tom Mendy <114578691+Tom-Mendy@users.noreply.github.com>
2023-08-10 15:47:46 +0200

    Update and rename auto-start.sh to .xsession

--- auto-start.sh
+++
@@ -1,9 +0,0 @@
-#!/ust/bin/sh
-
-nitrogen --restore
-xrandr -s 1920x1080
-
-. /home/tmendy/.nix-profile/etc/profile.d/nix.sh
-# Add nix app on dmenu
-export XDG_DATA_DIRS=~/.local/share/:~/.nix-profile/share:/usr/share
-cp -f ~/.nix-profile/share/applications/*.desktop ~/.local/share/applications/
