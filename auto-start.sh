a035b27ab4590e14bf35045c3c30900aa4f07260
Tom Mendy <114578691+Tom-Mendy@users.noreply.github.com>
2023-08-10 14:45:37 +0200

    Update auto-start.sh

--- auto-start.sh
+++ auto-start.sh
@@ -3,6 +3,7 @@
 nitrogen --restore
 xrandr -s 1920x1080
 
+. /home/tmendy/.nix-profile/etc/profile.d/nix.sh
 # Add nix app on dmenu
 export XDG_DATA_DIRS=~/.local/share/:~/.nix-profile/share:/usr/share
 cp -f ~/.nix-profile/share/applications/*.desktop ~/.local/share/applications/
