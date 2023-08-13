e72523668e7421c747dd870bb9d6eaee7a921f71
Tom Mendy <114578691+Tom-Mendy@users.noreply.github.com>
2023-08-13 19:09:13 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -69,6 +69,16 @@ echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] http
 sudo nala update
 sudo nala install brave-browser -y
 
+echo "VSCode"
+sudo nala install wget gpg -y
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+sudo nala install apt-transport-https -y
+sudo nala update
+sudo nala install code -y
+
 echo "Neovim"
 sudo nala install ninja-build gettext cmake unzip curl -y
 cd /tmp
@@ -93,9 +103,7 @@ sudo rm -r zsh_auto_install
 cd
 
 echo "ADD line to .zshrc"
-echo 'alias brave="com.brave.Browser"' >> ~/.zshrc
 echo 'alias Discord="com.discordapp.Discord"' >> ~/.zshrc
 echo 'alias spotify="com.spotify.Client"' >> ~/.zshrc
-echo 'alias code="com.visualstudio.code"' >> ~/.zshrc
 
 echo "Reboot Now"
