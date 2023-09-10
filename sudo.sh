b60666d854f305ed62b4543eaa8600b752f150a8
Tom Mendy <114578691+Tom-Mendy@users.noreply.github.com>
2023-09-10 14:10:33 +0200

    FIX sudo.sh

--- sudo.sh
+++ sudo.sh
@@ -1,15 +1,8 @@
 #!/usr/bin/bash
 
-if [ -z "$1" ] then
-  echo "replace YOUR_USERNAME by your username"
-  echo "./sudo.sh YOUR_USERNAME"
-  exit 1
-fi
-
 su -
-apt update && apt upgrade -y
 apt install sudo git -y
-usermod -aG sudo $1
+sudo usermod -aG sudo $USER
 echo "reboot and launch install.sh"
 sleep 2
-reboot
+sudo reboot
