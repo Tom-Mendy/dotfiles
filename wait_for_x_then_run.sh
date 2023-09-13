6d76b59a0cbb62f7ff773fbe1eda9a52f33f3032
Tom Mendy <114578691+Tom-Mendy@users.noreply.github.com>
2023-09-13 17:05:03 +0200

    UPD

---
+++ wait_for_x_then_run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+while :; do
+    pgrep Xorg
+    if [ $? -eq 0 ]; then
+        xrandr -s 1920x1080
+        $HOME/my_scripts/auto_wallpaper.sh
+        break
+    else
+        sleep 1
+    fi
+done
