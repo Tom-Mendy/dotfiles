171793e6869bbe253002e9eba7bd5f9afb3b170d
Tom Mendy <114578691+Tom-Mendy@users.noreply.github.com>
2023-09-13 14:55:48 +0200

    UPD

--- install.sh
+++ install.sh
@@ -121,14 +121,16 @@ chmod +x /tmp/rust.sh
 rm -f /tmp/rust.sh
 
 display "Nodejs"
-sudo nala update
-sudo nala install -y ca-certificates curl gnupg
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-NODE_MAJOR=20
-echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
-sudo nala update
-sudo nala install -y nodejs
+if [ "$(command -v npm)" ]; then
+  sudo nala update
+  sudo nala install -y ca-certificates curl gnupg
+  sudo mkdir -p /etc/apt/keyrings
+  curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+  NODE_MAJOR=20
+  echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+  sudo nala update
+  sudo nala install -y nodejs
+fi
 
 display "Python-add"
 sudo nala install -y python3-pip python3-venv
