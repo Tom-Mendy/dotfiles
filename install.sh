5927e9b169cc89bae3cac3c2031077f5462aa059
Tom Mendy <114578691+Tom-Mendy@users.noreply.github.com>
2023-09-30 17:33:11 +0200

    UPD

--- install.sh
+++ install.sh
@@ -264,11 +264,13 @@ display "Bing Wallpaper End"
 display "Docker Engine Start"
 if [ ! "$(command -v docker)" ]; then
   sudo nala update
-  sudo nala install -y ca-certificates curl gnupg
+  sudo nala update
+  sudo nala install ca-certificates curl gnupg
   sudo install -m 0755 -d /etc/apt/keyrings
   curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-  chmod a+r /etc/apt/keyrings/docker.gpg
-  echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  sudo chmod a+r /etc/apt/keyrings/docker.gpg
+  echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+    "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
   sudo nala update
   sudo nala install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
   if ! getent group docker >/dev/null; then
