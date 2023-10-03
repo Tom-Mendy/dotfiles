6ff50c902159e44ad525be6d5612ea2025fad434
Tom Mendy <114578691+Tom-Mendy@users.noreply.github.com>
2023-10-03 17:11:31 +0200

    UPD

--- install.sh
+++ install.sh
@@ -293,9 +293,11 @@ display "Theme Start"
 # Desktop Theme
 sudo nala install -y arc-theme
 # Icons
-git clone https://github.com/daniruiz/flat-remix.git /tmp/flat-remix
-sudo mv /tmp/flat-remix/Flat-Remix* /usr/share/icons/
-rm -rf /tmp/flat-remix
+if [ -z $(find /usr/share/icons/ -iname "Flat-Remix") ]; then
+  git clone https://github.com/daniruiz/flat-remix.git /tmp/flat-remix
+  sudo mv /tmp/flat-remix/Flat-Remix* /usr/share/icons/
+  rm -rf /tmp/flat-remix
+fi
 # Cursor
 sudo nala install -y inkscape
 git clone https://github.com/varlesh/oreo-cursors.git /tmp/oreo-cursors
