0efdbc174b80e08e7674c29742494cdba4242bdc
Tom Mendy <114578691+Tom-Mendy@users.noreply.github.com>
2023-10-04 21:21:14 +0200

    UPD

--- install.sh
+++ install.sh
@@ -389,7 +389,7 @@ display "Start Minikube"
 if [ ! "$(command -v minikube)" ]; then
   curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
   sudo install minikube-linux-amd64 /usr/local/bin/minikube
-  minikube start
+  sudo minikube start --force
 fi
 display "End Minikube"
 
