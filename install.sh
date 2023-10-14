8213c62744b80a449cc077cd712a5c23adf9749a
Tom Mendy <114578691+Tom-Mendy@users.noreply.github.com>
2023-10-14 22:28:06 +0200

    UPD

--- install.sh
+++ install.sh
@@ -267,7 +267,7 @@ display "End Media Player"
 display "Start Music Player"
 sudo flatpak install -y flathub com.spotify.Client
 # spotify_player
-sudo nala install libssl-dev libasound2-dev libdbus-1-dev
+sudo nala install -y libssl-dev libasound2-dev libdbus-1-dev
 cargo install spotify_player --features sixel,daemon
 display "End Music Player"
 
