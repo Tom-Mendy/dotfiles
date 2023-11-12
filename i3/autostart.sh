e5628a87f95199b798ec10a657637163df396af0
Tom Mendy <114578691+Tom-Mendy@users.noreply.github.com>
2023-11-12 19:52:09 +0100

    UPD

--- i3/autostart.sh
+++ i3/autostart.sh
@@ -13,24 +13,21 @@ killall blueman-applet
 blueman-applet &
 
 killall pulseaudio
-pulseaudio --start
-
-killall volumeicon
-volumeicon &
+pulseaudio --start &
 
 numlockx on &
 
 killall parcellite
-parcellite -d &
+parcellite &
 
 killall xautolock
 xautolock -time 10 -locker "i3lock-fancy -p" &
 
-xinput
-device_id=$(xinput | grep Touch | cut -f 2 | cut -d "=" -f 2)
-echo "$device_id"
-xinput set-prop $device_id 317 1
-xinput set-prop $device_id 294 1
+device_id=$(xinput |grep Touch |cut -f 2 |cut -d "=" -f 2)
+tap_to_clic_id=$(xinput --list-props $device_id |grep "Tapping Enabled" -m 1 |cut -d "(" -f 2 |cut -d ")" -f 1)
+xinput set-prop $device_id $tap_to_clic_id 1
+natural_scroll_id=$(xinput --list-props $device_id |grep "Natural Scrolling Enabled" -m 1 |cut -d "(" -f 2 |cut -d ")" -f 1)
+xinput set-prop $device_id $natural_scroll_id 1
 
 #refresh wallpaper at startup
 $HOME/my_scripts/auto_wallpaper.sh &
