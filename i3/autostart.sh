1fcd4139ebaeb5b7ca42a51ad87df7b5e6a18436
Tom Mendy <tom.mendy@epitech.eu>
2023-11-17 18:46:16 +0100

    [UPD] fix brightness

--- i3/autostart.sh
+++ i3/autostart.sh
@@ -24,10 +24,10 @@ killall xautolock
 xautolock -time 10 -locker "i3lock-fancy -p" &
 
 device_id=$(xinput |grep Touch |cut -f 2 |cut -d "=" -f 2)
-tap_to_clic_id=$(xinput --list-props $device_id |grep "Tapping Enabled" -m 1 |cut -d "(" -f 2 |cut -d ")" -f 1)
-xinput set-prop $device_id $tap_to_clic_id 1
-natural_scroll_id=$(xinput --list-props $device_id |grep "Natural Scrolling Enabled" -m 1 |cut -d "(" -f 2 |cut -d ")" -f 1)
-xinput set-prop $device_id $natural_scroll_id 1
+tap_to_clic_id=$(xinput --list-props "$device_id" |grep "Tapping Enabled" -m 1 |cut -d "(" -f 2 |cut -d ")" -f 1)
+xinput set-prop "$device_id" "$tap_to_clic_id" 1
+natural_scroll_id=$(xinput --list-props "$device_id" |grep "Natural Scrolling Enabled" -m 1 |cut -d "(" -f 2 |cut -d ")" -f 1)
+xinput set-prop "$device_id" "$natural_scroll_id" 1
 
 #refresh wallpaper at startup
-$HOME/my_scripts/auto_wallpaper.sh &
+"$HOME/my_scripts/auto_wallpaper.sh" &
