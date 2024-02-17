adf8487201918a1d37a01fe347a5f0dcc0a05b9d
Tom Mendy <114578691+Tom-Mendy@users.noreply.github.com>
2024-02-17 10:51:11 +0100

    [FIX] zsh config

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -1,6 +1,19 @@
-export EDITOR=nvim
-export TERM=xterm-kitty
+# to make nvim default editor
+if [ "$(command -v nvim)" ]; then
+    export EDITOR=nvim
+fi
+# to make kitty default editor
+if [ "$(command -v kitty)" ]; then
+    export TERM=xterm-kitty
+fi
+
+if [ "$(command -v cargo)" ]; then
+    export PATH=$HOME/.cargo/bin:$PATH
+fi
+if [ "$(command -v flatpak)" ]; then
+    export PATH=/var/lib/flatpak/exports/bin:$PATH
+fi
+# to make bin in $HOME/my_scripts in the PATH
 export PATH=$HOME/my_scripts:$PATH
-export PATH=$HOME/.cargo/bin:$PATH
-export PATH=/var/lib/flatpak/exports/bin:$PATH
+# to make bin in /usr/bin in the PATH
 export PATH=/usr/bin:$PATH
