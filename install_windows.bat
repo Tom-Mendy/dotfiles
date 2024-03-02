c5bc29293315f66f8ab35bf9d121d1e6e12bc442
Tom Mendy <114578691+Tom-Mendy@users.noreply.github.com>
2024-03-02 19:28:12 +0100

    Add files via upload

---
+++ install_windows.bat
@@ -0,0 +1,139 @@
+:: Windows Package Manager
+:: my personal list of winget packages to install
+
+:: Browsers
+  :: Edge
+  winget install Microsoft.Edge
+  ::Firefox
+  winget install Mozilla.Firefox
+  :: Chrome
+  winget install Google.Chrome
+  :: Opera
+  ::winget install Opera.OperaGX
+  :: Brave
+  ::winget install BraveSoftware.BraveBrowser
+  :: Vivaldi
+  ::winget install VivaldiTechnologies.Vivaldi
+  :: Tor
+  winget install TheTorProject.TorBrowser
+  :: Chromium
+  ::winget install Chromium.Chromium
+
+:: Communication Tools
+  :: Discord
+  winget install Discord.Discord
+  :: Microsoft Teams
+  winget install Microsoft.Teams
+  :: Zoom
+  winget install Zoom.Zoom
+  :: Slack
+  winget install SlackTechnologies.Slack
+
+:: Media Players
+  :: VLC
+  winget install VideoLAN.VLC
+  :: Spotify
+  ::winget install Spotify.Spotify
+  :: OBS
+  winget install OBSProject.OBSStudio
+  :: Audacity
+  winget install Audacity.Audacity
+
+:: Development Tools
+  :: Visual Studio Code
+  winget install Microsoft.VisualStudioCode
+  :: Git
+  winget install Git.Git
+  :: Node.js
+  winget install OpenJS.NodeJS
+  :: Python
+  winget install Python.Python
+  :: Java
+  winget install OpenJDK.Java
+  :: Docker
+  winget install Docker.DockerDesktop
+  :: PuTTY
+  winget install SimonTatham.Putty
+  :: VM Software
+    :: VirtualBox
+    winget install Oracle.VirtualBox
+    :: VMware
+    winget install VMware.VMwareWorkstationPro
+
+:: Utilities
+  :: 7-Zip
+  winget install 7zip.7zip
+  :: Notepad++
+  winget install NotepadPlusPlus.NotepadPlusPlus
+  :: AutoHotKey
+  winget install AutoHotkey.AutoHotkey
+  :: Everything
+  winget install voidtools.Everything
+  :: PowerToys
+  winget install Microsoft.PowerToys
+  :: Bitwarden
+  winget install Bitwarden.Bitwarden
+  :: Microsoft Office
+  winget install Microsoft.Office
+  :: Sreen Sharing
+  :: TeamViewer
+  winget install TeamViewer.TeamViewer
+  :: AnyDesk
+  winget install AnyDesk.AnyDesk
+
+:: System Monitoring
+  :: HWMonitor
+  winget install CPUID.HWMonitor
+  :: CPU-Z
+  winget install CPUID.CPUZ
+  :: MSI Afterburner
+  winget install MSI.Afterburner
+  :: Disk Monitoring
+    :: CrystalDiskInfo
+    winget install CrystalDiskInfo.CrystalDiskInfo
+    :: Speccy
+    winget install Piriform.Speccy
+
+:: Gaming
+  :: GeForce Experience
+  winget install NVIDIA.GeForceExperience
+  :: AMD Adrenalin
+  winget install AMD.AMDSoftware
+
+:: Peripherals Software
+  :: Logitech G Hub
+  winget install Logitech.GHUB
+  :: Razer Synapse
+  winget install Razer.Synapse
+  :: SteelSeries GG
+  winget install SteelSeries.GG
+  :: HyperX NGenuity
+  winget install HyperX.NGenuity
+
+:: Games
+  :: Roblox
+  winget install Roblox.Roblox
+  :: Minecraft
+    :: Minecraft Launcher
+    winget install Mojang.MinecraftLauncher
+    :: Badlion Client
+    winget install Badlion.BadlionClient
+    :: multiMC
+    winget install MultiMC.MultiMC
+  :: Game Launchers
+    :: Steam
+    winget install Valve.Steam
+    :: Epic Games
+    winget install EpicGames.EpicGamesLauncher
+    :: Riot Launcher
+    winget install RiotGames.RiotClient
+    :: Ubisoft Connect
+    winget install Ubisoft.Connect
+    :: Battle.net
+    winget install Blizzard.BattleNet
+    :: Origin
+    winget install ElectronicArts.Origin
+    :: GOG
+    winget install GOG.Galaxy
+    :: Oculus
+    winget install Oculus.Oculus
