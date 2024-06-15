8a5da61675dcd533525638bf57918920eed77ace
Tom Mendy <114578691+Tom-Mendy@users.noreply.github.com>
2024-06-15 22:26:06 +0200

    [ADD] nix config

---
+++ install_nixos.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+
+sudo cp "${SCRIPT_DIR}/nix/configuration.nix" "/etc/nixos/configuration.nix"
+
+sudo nix-channel --add https://github.com/nix-community/home-manager/archive/release-24.05.tar.gz home-manager
+sudo nix-channel --update
+
+rustup default stable
+sudo rustup default stable
