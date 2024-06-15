b2564b0cd9c59823f31165afcf2ead494cd8e5f7
Tom Mendy <114578691+Tom-Mendy@users.noreply.github.com>
2024-06-15 23:04:25 +0200

    [UPD] nix config

--- install_nixos.sh
+++ install_nixos.sh
@@ -7,5 +7,7 @@ sudo cp "${SCRIPT_DIR}/nix/configuration.nix" "/etc/nixos/configuration.nix"
 sudo nix-channel --add https://github.com/nix-community/home-manager/archive/release-24.05.tar.gz home-manager
 sudo nix-channel --update
 
+sudo nixos-rebuild switch
+
 rustup default stable
 sudo rustup default stable
