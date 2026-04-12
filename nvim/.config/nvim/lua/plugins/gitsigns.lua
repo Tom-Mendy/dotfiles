caf3125c5d741db3a3bb165e55294e639ffe282c
Tom-Mendy <tom.mendy@epitech.eu>
2026-04-12 10:22:48 +0900

    refactor: streamline plugin configurations for gitsigns, neo-tree, nvim-treesitter, and telescope

--- nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -1,22 +1,19 @@
 return {
   -- Adds git related signs to the gutter, as well as utilities for managing changes
   'lewis6991/gitsigns.nvim',
-  opts = {
-    -- See `:help gitsigns.txt`
-    signs = {
-      add = { text = '+' },
-      change = { text = '~' },
-      delete = { text = '_' },
-      topdelete = { text = '‾' },
-      changedelete = { text = '~' },
-    },
-    on_attach = function(bufnr)
+  opts = function(_, opts)
+    local previous_on_attach = opts.on_attach
+    opts.on_attach = function(bufnr)
+      if previous_on_attach then
+        previous_on_attach(bufnr)
+      end
+
       vim.keymap.set('n', '<leader>gp', require('gitsigns').prev_hunk,
         { buffer = bufnr, desc = '[G]o to [P]revious Hunk' })
       vim.keymap.set('n', '<leader>gn', require('gitsigns').next_hunk,
         { buffer = bufnr, desc = '[G]o to [N]ext Hunk' })
       vim.keymap.set('n', '<leader>ph', require('gitsigns').preview_hunk,
         { buffer = bufnr, desc = '[P]review [H]unk' })
-    end,
-  },
+    end
+  end,
 }
--- nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,14 +1,13 @@
 return {
 	-- Highlight, edit, and navigate code
 	"nvim-treesitter/nvim-treesitter",
-	dependencies = {
-		"nvim-treesitter/nvim-treesitter-textobjects",
-		"nvim-treesitter/nvim-treesitter-context",
-	},
-	build = ":TSUpdate",
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "jsonc" },
-		})
+	opts = function(_, opts)
+		opts.ensure_installed = opts.ensure_installed or {}
+		local wanted = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "jsonc" }
+		for _, lang in ipairs(wanted) do
+			if not vim.tbl_contains(opts.ensure_installed, lang) then
+				table.insert(opts.ensure_installed, lang)
+			end
+		end
 	end,
 }
