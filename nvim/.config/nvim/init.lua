a8e317a5a39717431a3eb682df5fb32ad8185350
Tom-Mendy <tom.mendy@epitech.eu>
2026-04-12 10:19:59 +0900

    feat: enhance Neovim configuration with LazyVim integration and add rollback strategy

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -15,4 +15,31 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins")
+require("lazy").setup({
+	spec = {
+		{ "LazyVim/LazyVim", import = "lazyvim.plugins" },
+		{ import = "plugins" },
+	},
+	defaults = {
+		lazy = false,
+		version = false,
+	},
+	install = {
+		colorscheme = { "tokyonight", "habamax" },
+	},
+	checker = {
+		enabled = true,
+		notify = false,
+	},
+	performance = {
+		rtp = {
+			disabled_plugins = {
+				"gzip",
+				"tarPlugin",
+				"tohtml",
+				"tutor",
+				"zipPlugin",
+			},
+		},
+	},
+})
